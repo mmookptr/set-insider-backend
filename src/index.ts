fd70dac3470c43a6dc802dbaf0236c6f0500bbb4
mmookptr <patteera.lik@ku.th>
2020-12-19 17:44:16 +0700

    implement getting port and database url from env

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,37 @@
 import express from "express"
 import "reflect-metadata"
-import { createConnection } from "typeorm"
+import {
+  createConnection,
+  ConnectionOptions,
+  getConnectionOptions,
+} from "typeorm"
 import { SETInsiderApplication } from "./SETInsiderApplication"
 import { createRouter } from "./router"
 import cors from "cors"
+import dotenv from "dotenv"
+
+dotenv.config()
+
+async function getOptions(): Promise<ConnectionOptions> {
+  let connectionOptions: ConnectionOptions
+  connectionOptions = {
+    type: "postgres",
+    synchronize: true,
+    logging: false,
+    entities: ["dist/entity/*.*"],
+  }
+
+  if (process.env.DATABASE_URL) {
+    Object.assign(connectionOptions, { url: process.env.DATABASE_URL })
+  } else {
+    // gets your default configuration
+    // you could get a specific config by name getConnectionOptions('production')
+    // or getConnectionOptions(process.env.NODE_ENV)
+    connectionOptions = await getConnectionOptions()
+  }
+
+  return connectionOptions
+}
 
 createConnection()
   .then((connection) => {
